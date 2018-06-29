9b99ebbac0509664ae531fc56e489b02d863a8d8
briandoyle81 <briandoyle81@gmail.com>
2018-06-29 15:55:36 -0600

    changed default database

--- djorg/settings.py
+++ djorg/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
